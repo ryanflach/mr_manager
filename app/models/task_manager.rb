0daf6d54778b4d8ced643065d7fda89e38b738f7
Ryan Flach <ryanflach@users.noreply.github.com>
2016-06-24 09:47:11 -0700

    Added functionality to TaskManager and TaskManagerApp for displaying tasks

--- app/models/task_manager.rb
+++ app/models/task_manager.rb
@@ -9,10 +9,20 @@ class TaskManager
 
   def create(task)
     database.transaction do
-      databse['tasks'] ||= []
+      database['tasks'] ||= []
       database['total'] ||= 0
       database['total'] += 1
       database['tasks'] << { "id" => database['total'], "title" => task[:title], "description" => task[:description] }
     end
+  end
+
+  def raw_tasks
+    database.transaction do
+      database['tasks'] || []
+    end
+  end
+
+  def all
+    raw_tasks.map { |data| Task.new(data) }
   end
 end
